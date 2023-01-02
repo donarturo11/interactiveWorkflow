8cfc8244ed55441ed6807e0d498c0563d8e55cac
Artur Wrona <arturwrona91@gmail.com>
2023-01-02 17:54:29 +0100

    Add set_env.sh

---
+++ scripts/aqt/set_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -xe
+export QT_VERSION=6.4.0
+export QT_PATH=/opt/Qt
+export QT_MACOS=${QT_PATH}/${QT_VERSION}/macos
+export AQT_VERSION="aqtinstall==3.0.1"
+export PATH=$PATH:${QT_PATH}/Tools/CMake/bin:${QT_PATH}/Tools/Ninja:${QT_PATH}/${QT_VERSION}/macos/bin:/opt/osxcross/bin
